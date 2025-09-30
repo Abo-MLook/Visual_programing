ba1f2cff9e09887e6f079b444bcd1055be21c59f
Marwan Fahad <mamemo1270@gmail.com>
2025-09-30 20:13:08 +0300

    update

---
+++ Visual_programming1/src/main/java/basics_oop/Thoretical/CastingExample.java
@@ -0,0 +1,65 @@
+
+package basics_oop.Thoretical;
+
+//slide  31
+
+class Point {
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Method to translate the point by dx and dy
+    void translate(int dx, int dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    // Method to get the X coordinate
+    int getX() {
+        return x;
+    }
+}
+
+// The `Object` class is the root class of all Java classes. Every class in Java, 
+// directly or indirectly, inherits from `Object`. An `Object` reference can 
+// store a reference to any type of object. However, to access subclass-specific methods, 
+// you must cast the `Object` reference to the appropriate class type.
+
+public class CastingExample {
+    public static void main(String[] args) {
+        // Object o1 is an Object reference, but it points to a Point object
+        Object o1 = new Point(5, -3); // Point object is created and assigned to o1
+
+        // Casting o1 to Point to use methods specific to the Point class
+        ((Point) o1).translate(6, 2);  // This works, since o1 refers to a Point object
+
+        // Now, we store the casted object into a Point reference for easier access
+        Point p = (Point) o1;
+
+        // Accessing the Point method getX() after casting
+        int x = p.getX(); // This works, because o1 was cast to Point
+
+        // Printing the updated X coordinate
+        System.out.println("Updated x: " + x);  // Outputs: Updated x: 11
+
+        // Object o2 is an Object reference pointing to a String object
+        Object o2 = "hello there"; // o2 points to a String object
+
+        // Casting o2 to String and calling String's length() method
+        int len = ((String) o2).length();  // Works fine since o2 refers to a String object
+
+        // Printing the length of the string
+        System.out.println("Length of the string: " + len);  // Outputs: Length of the string: 11
+
+        // Example where casting might fail: trying to cast String to Point
+        try {
+            Point invalidPoint = (Point) o2;  // This will throw ClassCastException
+        } catch (ClassCastException e) {
+            System.out.println("ClassCastException occurred: " + e);
+        }
+    }
+}
+
---
+++ Visual_programming1/src/main/java/basics_oop/Thoretical/Polymorphism_slide44.java
@@ -0,0 +1,57 @@
+
+package basics_oop.Thoretical;
+
+// slide 33 - 34
+
+// Base class Employee
+class Employee {
+    // Method defined in Employee
+    public void work() {
+        System.out.println("Employee working...");
+    }
+
+    // Another method specific to Employee
+    public void takeBreak() {
+        System.out.println("Employee taking a break...");
+    }
+}
+
+// Derived class Lawyer extends Employee
+class Lawyer extends Employee {
+    // Method specific to Lawyer
+    public void sue() {
+        System.out.println("Lawyer suing...");
+    }
+
+    // Overriding the work method to show polymorphism
+    @Override
+    public void work() {
+        System.out.println("Lawyer working on a case...");
+    }
+}
+
+
+//Employee ed = new Lawyer(); so here for all are the same 
+//way I can call any method that is exist in Employee and exist in both,
+//but if only method exist on Lawyer , I can not call it 
+public class Polymorphism_slide44 {
+    public static void main(String[] args) {
+        // Polymorphism: Employee reference, but a Lawyer object
+        Employee ed = new Lawyer();
+
+        // Calling method from Employee class (works fine)
+        ed.work(); // Output: Lawyer working on a case...
+        ed.takeBreak(); // Output: Employee taking a break...
+
+        // The following would give a compiler error, because `sue()` is not in the Employee class:
+        // ed.sue(); // Compiler error: cannot find symbol
+
+        // To call the `sue()` method (which is specific to Lawyer), we need to cast `ed` to `Lawyer`
+        ((Lawyer) ed).sue(); // Output: Lawyer suing...
+        
+        // Attempt to cast to an incorrect type (casting to a non-existent class)
+        // This will throw a runtime error if the cast is not correct
+        // Object someObject = new Employee();
+        // ((Lawyer) someObject).sue(); // Runtime error: ClassCastException
+    }
+}
