66f6ed3c5436293250f108b957cae9758606789f
Marwan Fahad <mamemo1270@gmail.com>
2025-12-14 22:14:04 +0300

    Update L2_excersice.java

--- Visual_programming1/src/main/java/ch2/Dialog/L2_excersice.java
+++ Visual_programming1/src/main/java/ch2/Dialog/L2_excersice.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ch2.Dialog;
 
-/**
- *
- * @author memom
- */
+
+
+package ch2.Dialog;
+import javax.swing.*;
+
 public class L2_excersice {
     
+    
+    public static void main(String[] args) {
+        
+        int checker = 0;
+        
+        while (checker ==0) {
+            try {
+             String n1 = JOptionPane.showInputDialog(null,"enter number1 = ","n1",JOptionPane.INFORMATION_MESSAGE);
+            String n2 = JOptionPane.showInputDialog(null,"enter number2 = ","n2",JOptionPane.INFORMATION_MESSAGE);
+            int sum =  Integer.parseInt(n1) + Integer.parseInt(n2);
+            
+            JOptionPane.showMessageDialog(null, n1 + " + "+n2+" = "+sum);
+            
+            // if press   yes give 0 , if no give 1 
+            checker = JOptionPane.showConfirmDialog(null, "you want to continue ? ","conform",JOptionPane.YES_NO_OPTION);
+             
+                
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "You entered wrong data type","Eror window",JOptionPane.ERROR_MESSAGE);
+                            checker = JOptionPane.showConfirmDialog(null, "you want to continue ? ","conform",JOptionPane.YES_NO_OPTION);
+
+            }
+      
+                        
+
+            
+            
+        }
+        
+        
+    }
 }
