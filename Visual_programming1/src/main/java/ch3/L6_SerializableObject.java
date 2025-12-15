a5ffdea9f55d06c0c521540f3b9135212fc556cc
Marwan Fahad <mamemo1270@gmail.com>
2025-12-16 00:13:50 +0300

    a

---
+++ Visual_programming1/src/main/java/ch3/L6_SerializableObject.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ch3;
+
+/**
+ *
+ * @author memom
+ */
+public class L6_SerializableObject {
+    
+}
